f3574a4073355308cd756e49b16789289a7da4ec
ChinCheungLam <131918625+ChinCheungLam@users.noreply.github.com>
2023-11-17 20:09:52 -0800

    Create customer.h

---
+++ customer.h
@@ -0,0 +1,25 @@
+#ifndef CUSTOMER_H_
+#define CUSTOMER_H_
+#include <string>
+#include <vector>
+#include<iostream>
+#include"dataVerifier.h"
+using namespace std;
+
+class Customer {
+public:
+    Customer(int customerId, const string& lastName, const string& firstName);
+    int getCustomerId() const;
+    string getLastName() const;
+    string getFirstName() const;
+    void addTransaction(dataVerifier* transaction);
+    const vector<dataVerifier*>& getTransactions() const;
+    void displayHistory() const;
+
+private:
+    int customerId_;
+    string lastName_;
+    string firstName_;
+    Hash<int, Customer*> customers_;
+};
+#endif
