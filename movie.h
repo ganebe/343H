38190d1d305b5bfc7795b3becac3ab75948e1648
ChinCheungLam <131918625+ChinCheungLam@users.noreply.github.com>
2023-11-18 18:42:31 -0800

    Update movie.h
    Add comments

--- movie.h
+++ movie.h
@@ -7,16 +7,28 @@ using namespace std;
 
 class Movie {
 public:
+
+    // Constructor	
     Movie(int stock, const string& director, const string& title);
+
+    // Virtual destructor
     virtual ~Movie() = default;
 
+    // get stock
     int getStock() const;
+
+    // set stock
     void setStock(int stock);
+
+    // get current stock
     int getCurrentStock()const;
+
+    // get director
     const string& getDirector() const;
+    // get title
     const string& getTitle() const;
 
-
+    // borrowing and returning functions
     bool borrowMovie();
     bool returnMovie();
 
@@ -26,8 +38,8 @@ public:
 	virtual bool operator<(const Movie& other) const;
 	virtual bool operator>(const Movie& other) const;
 private:
-    int current_stock_;
-    int stock_;
-    string director_, title_;
+    int current_stock_; // Number of stock available.
+    int stock_; // Number of stock.
+    string director_, title_; // Director of the movie and title of the movie.
 };
 #endif
