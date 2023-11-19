621de5e899765622542ba3d25e2f1636bf58c18f
ChinCheungLam <131918625+ChinCheungLam@users.noreply.github.com>
2023-11-18 18:35:57 -0800

    Update customer.h
    Add comments

--- customer.h
+++ customer.h
@@ -7,17 +7,28 @@ using namespace std;
 
 class Customer {
 public:
+    // Constructor
     Customer(int customerId, const string& lastName, const string& firstName);
+
+    // Get the customer's ID
     int getCustomerId() const;
+
+    // Get the customer's last name.
     string getLastName() const;
+
+    // Get the customer's first name.
     string getFirstName() const;
+
+    // Adds a transaction to the customer's history.
     void addHistory(string data);
+
+    // Displays the customer's transaction history.
     void displayHistory() const;
 
 private:
-    int customerId_;
-    string lastName_;
-    string firstName_;
-    vector<string> history();
+    int customerId_; // Unique identifier for the customer.
+    string lastName_; // Last name of the customer.
+    string firstName_; // First name of the customer.
+    vector<string> history(); // customer's transactions history
 };
 #endif
